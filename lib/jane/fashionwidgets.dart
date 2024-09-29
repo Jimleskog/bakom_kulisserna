4bcf21f79f0ec811746e60a3f693ec1a30ba3836
Jim Lewis Skoglund <124883061+Jimleskog@users.noreply.github.com>
2024-09-29 16:25:01 +0200

    Refactor fashionWidgets.dart to update the text for the "SWEATER VIBES" widget

--- lib/jane/fashionwidgets.dart
+++ lib/jane/fashionwidgets.dart
@@ -15,7 +15,7 @@ List<Widget> fashionWidgets(ScrollController controller) {
     buildPictureWholeScreeenWidget(
         buildAssetString('pic.jpg'),
         'SWEATER VIBES',
-        'Mjukt, nyttigt oversize - årets skönaste trend för sommaren test',
+        'Mjukt, nyttigt oversize - årets skönaste trend för sommaren',
         'SHOPPA NU'),
     buildsmallContainerBanner('Köp nu - betala senare'),
     Padding(
